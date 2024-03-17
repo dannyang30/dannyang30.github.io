14609807a192479c44196c8211fc3b16eae8fd72
dannyang30 <dannyang30@hotmail.com>
2024-03-17 17:06:46 +0800

    Made header sticky

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
 function showMobileMenu(){
     const mobilemenu = document.querySelector('.navbar-menu ul')
     mobilemenu.style.display = 'flex'
-
+        
     const menuicon = document.querySelector('#menu-icon')
     menuicon.style.display = 'none'
 
     const closeicon = document.querySelector('#close-icon')
     closeicon.style.display = 'block'
-
-    const hero = document.querySelector('.hero')
-    const projects = document.querySelector('.projects')
-    const contact = document.querySelector('.contact')
-    const footer = document.querySelector('.footer')
-
-    hero.style.display = 'none'
-    projects.style.display = 'none'
-    contact.style.display = 'none'
-    footer.style.display = 'none'
-
 }
 
 function hideMobileMenu(){
